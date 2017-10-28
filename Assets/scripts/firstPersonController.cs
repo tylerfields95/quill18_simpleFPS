9726be1371f340546f15b174f10613a93c2ea1ee
tylerfields95 <tylerfields95@gmail.com>
2017-10-28 01:49:58 -0400

    finished part 2 camera and player movement

--- Assets/scripts/firstPersonController.cs
+++ Assets/scripts/firstPersonController.cs
@@ -3,14 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class firstPersonController : MonoBehaviour {
+    public float movementSpeed = 5.0f;
+    public float mouseSensitivity = 5.0f;
+
+    float verticleRotation = 0;
+    public float upDownRange = 60.0f;
 
 	// Use this for initialization
 	void Start () {
-		
+        //locks cursor to the game
+        Cursor.lockState = CursorLockMode.Locked;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        //rotation
+        float rotLeftRight = Input.GetAxis("Mouse X") * mouseSensitivity;
+        transform.Rotate(0, rotLeftRight, 0);
+
+        verticleRotation -= Input.GetAxis("Mouse Y") * mouseSensitivity;
+        verticleRotation = Mathf.Clamp(verticleRotation, -upDownRange, upDownRange);
+        Camera.main.transform.localRotation = Quaternion.Euler(verticleRotation, 0, 0);
+
+        //movement
+        float forwardSpeed = Input.GetAxis("Vertical") * movementSpeed;
+        float sideSpeed = Input.GetAxis("Horizontal") * movementSpeed;
+        Vector3 speed = new Vector3(sideSpeed, 0, forwardSpeed);
+
+        speed = transform.rotation * speed;
+
+        CharacterController cc = GetComponent<CharacterController>();
+
+        cc.SimpleMove(speed);
+
 	}
 }
